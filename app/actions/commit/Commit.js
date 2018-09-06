c105a3c5971bf507e223be46c6ef302c535cd662
Tom Atnip <tom.atnip@gmail.com>
2018-09-05 21:02:59 -0400

    Delete all files
    Deleting all files to start from scratch and learn how everything
    works from the ground up. Was using template repo, but did not
    understand how it was put together.

--- app/actions/commit/Commit.js
+++
@@ -1,9 +0,0 @@
-// @flow
-export const ADD_COMMIT = 'ADD_COMMIT';
-
-export function addCommit(commit) {
-  return { 
-    ...commit, 
-    type: ADD_COMMIT 
-  };
-}
--- app/actions/commit/index.js
+++
@@ -1,7 +0,0 @@
-import { addCommit, ADD_COMMIT } from './Commit';
-
-export
-    {
-        addCommit,
-        ADD_COMMIT
-    }
